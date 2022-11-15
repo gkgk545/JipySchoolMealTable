dc187b5f3e48f805fde51c6beac42ef7a33e5ab7
jhlee <gkgk545@gmail.com>
2022-11-15 23:00:02 +0900

    for gh-pages part4

--- src/App.js
+++ src/App.js
@@ -1,9 +1,43 @@
 import Meal from "./Meal";
+import { useEffect, useState } from "react";
+
+const KEY = process.env.REACT_APP_API_KEY;
+let today = null;
 
 function App() {
+  const [mealData, setMealData] = useState([]);
+  const [state, setState] = useState(false);
+
+  const getMealData = async () => {
+    let day = new Date();
+    let year = day.getFullYear();
+    let month = day.getMonth() + 1;
+    let date = day.getDate();
+    today =
+      `${year}`.substring(2, 4) +
+      `${month}` +
+      `${date < 10 ? `0${date}` : date}`;
+    try {
+      const data = await (
+        await fetch(
+          `https://open.neis.go.kr/hub/mealServiceDietInfo?KEY=${KEY}&Type=json&pIndex=1&pSize=1&` +
+            `ATPT_OFCDC_SC_CODE=J10&SD_SCHUL_CODE=7731063&MLSV_YMD=${today}`
+        )
+      ).json();
+      setState(true);
+      setMealData(data.mealServiceDietInfo[1].row[0].DDISH_NM.split("<br/>"));
+    } catch (error) {
+      setState(false);
+    }
+  };
+
+  useEffect(() => {
+    getMealData();
+  }, []);
+
   return (
     <div>
-      <Meal />
+      <Meal state={state} mealData={mealData} today={today} />
     </div>
   );
 }
--- src/Meal.js
+++ src/Meal.js
@@ -1,53 +1,62 @@
-import { useEffect, useState } from "react";
+import "./reset.css";
+import styled from "styled-components";
 
-const KEY = process.env.REACT_APP_API_KEY;
+const Background = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-color: rgba(255, 255, 0, 0.5);
+  font-family: "Hahmlet", serif;
+`;
 
-const Meal = () => {
-  const [mealData, setMealData] = useState([]);
-  const [state, setState] = useState(false);
+const Title = styled.h1`
+  font-size: 60px;
+  font-weight: 800;
+  margin-bottom: 5vh;
+`;
 
-  let day = new Date();
-  let year = day.getFullYear();
-  let month = day.getMonth() + 1;
-  let date = day.getDate();
-  let today =
-    `${year}`.substring(2, 4) + `${month}` + `${date < 10 ? `0${date}` : date}`;
+const Table = styled.div`
+  height: 50vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+`;
 
-  const getMealData = async () => {
-    try {
-      const data = await (
-        await fetch(
-          `https://open.neis.go.kr/hub/mealServiceDietInfo?KEY=${KEY}&Type=json&pIndex=1&pSize=1&` +
-            `ATPT_OFCDC_SC_CODE=J10&SD_SCHUL_CODE=7731063&MLSV_YMD=${today}`
-        )
-      ).json();
-      setState(true);
-      setMealData(data.mealServiceDietInfo[1].row[0].DDISH_NM.split("<br/>"));
-    } catch (error) {
-      setState(false);
-    }
-  };
+const Menu = styled.span`
+  font-size: 50px;
+`;
 
-  useEffect(() => {
-    getMealData();
-  }, []);
-
+const Meal = ({ state, mealData }) => {
   return (
-    <div>
-      <h1>지평초 오늘의 식단</h1>
-      {state ? (
-        mealData.map((item) => (
-          <div key={Math.random()}>
-            <h2>{item.replace(/(0|1|2|3|4|5|6|7|8|9|@|\*|\.|\(|\))/g, "")}</h2>
-            <br />
+    <Background>
+      <style>
+        @import
+        url('https://fonts.googleapis.com/css2?family=Hahmlet:wght@400;800&display=swap');
+      </style>
+      <Title>
+        지평초 오늘({new Date().getMonth() + 1}월 {new Date().getDate()}일)의
+        식단
+      </Title>
+      <Table>
+        {state ? (
+          mealData.map((item) => (
+            <div key={Math.random()}>
+              <Menu>
+                {item.replace(/(0|1|2|3|4|5|6|7|8|9|@|\*|\.|\(|\))/g, "")}
+              </Menu>
+              <br />
+            </div>
+          ))
+        ) : (
+          <div>
+            <Title>오늘은 급식을 하지 않습니다...</Title>
           </div>
-        ))
-      ) : (
-        <div>
-          <h1>오늘은 급식을 하지 않습니다...</h1>
-        </div>
-      )}
-    </div>
+        )}
+      </Table>
+    </Background>
   );
 };
 
